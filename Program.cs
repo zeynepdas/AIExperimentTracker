51b8195f934eff6a61865b922fa978a3a8b36d25
Zeynep Daş <zeynepdasbusiness@gmail.com>
2026-01-02 22:20:37 +0300

    Initial .NET 9 Web API setup with Swagger and Minimal API

---
+++ Program.cs
@@ -0,0 +1,18 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Swagger services
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Swagger middleware
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
